dfc3ab96ac8dcfefaa531382806938120a49f7a1
DGE83 <jerome5783@gmail.com>
2025-09-09 21:16:22 +0200

    Mise à jour des dernières créations

---
+++ 1-start-public.bat
@@ -0,0 +1,7 @@
+@echo off
+title Portfolio Public (Frontend)
+echo Lancement du serveur pour le portfolio public...
+echo Ce terminal surveille vos fichiers dans 'frontend_src' et met a jour le site en direct.
+echo.
+npm run dev
+pause
---
+++ 2-start-admin.bat
@@ -0,0 +1,6 @@
+@echo off
+title Serveur Administration (Backend)
+echo Lancement du serveur pour la page d'administration...
+echo.
+npm start
+pause
